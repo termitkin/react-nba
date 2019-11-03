bed2e32c2dde55d2161f7d9c9720ee5f20b69dfa
termitkin <bigos911@gmail.com>
2019-11-03 20:29:21 +0300

    added 404 page

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Provider } from "react-redux";
 import configureStore from "./store/configureStore";
-import { HashRouter as Router, Route } from "react-router-dom";
-// import PropTypes from "prop-types";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import IndexPage from "./pages/IndexPage";
 import TeamPage from "./pages/TeamPage";
 import AllTeams from "./pages/AllTeams";
+import Page404 from "./pages/Page404";
 
 const store = configureStore();
 
@@ -13,26 +13,25 @@ const AppRouter = () => {
   return (
     <Provider store={store}>
       <Router>
-        <Route
-          path="/"
-          exact
-          render={props => <IndexPage params={{ ...props }} />}
-        />
-        <Route
-          path="/team=:id"
-          render={props => <TeamPage params={{ ...props }} />}
-        />
-        <Route
-          path="/allteams"
-          render={props => <AllTeams params={{ ...props }} />}
-        />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={props => <IndexPage params={{ ...props }} />}
+          />
+          <Route
+            path="/team=:id"
+            render={props => <TeamPage params={{ ...props }} />}
+          />
+          <Route
+            path="/all-teams"
+            render={props => <AllTeams params={{ ...props }} />}
+          />
+          <Route path="*" render={() => <Page404 />} />
+        </Switch>
       </Router>
     </Provider>
   );
 };
-
-// AppRouter.propTypes = {
-//   store: PropTypes.object.isRequired
-// };
 
 export default AppRouter;
---
+++ src/pages/Page404.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Page404 = () => {
+  return (
+    <div className="wrapper">
+      <Header />
+      <section>
+        <h1 className="heading">Error 404: Page not found</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Page404;
