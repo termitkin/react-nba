2aae876eff7385e627c6da2c0044672231df6101
termitkin <bigos911@gmail.com>
2019-12-31 15:37:09 +0000

    Deploying to gh-pages from master ec07f1398dc4f9986e3f07006c38252bc128dcd6

---
+++ precache-manifest.29fb7483bc973e586c5579b4e71f6ab6.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62bba1846e8785decad27c3100c49357",
+    "url": "/react-nba/index.html"
+  },
+  {
+    "revision": "d23a7de42074dde027e7",
+    "url": "/react-nba/static/css/2.c6231af7.chunk.css"
+  },
+  {
+    "revision": "32a3180bb3be80cddf11",
+    "url": "/react-nba/static/css/main.58e8412f.chunk.css"
+  },
+  {
+    "revision": "d23a7de42074dde027e7",
+    "url": "/react-nba/static/js/2.6979e290.chunk.js"
+  },
+  {
+    "revision": "32a3180bb3be80cddf11",
+    "url": "/react-nba/static/js/main.899a7a57.chunk.js"
+  },
+  {
+    "revision": "ce86678f82a3c9d212ba",
+    "url": "/react-nba/static/js/runtime-main.28e88c14.js"
+  },
+  {
+    "revision": "ea153372fa35e737e68ea7d5a184e57f",
+    "url": "/react-nba/static/media/ATL.ea153372.svg"
+  },
+  {
+    "revision": "2ed481f108e1d53c8e973e46ff1b1f07",
+    "url": "/react-nba/static/media/BKN.2ed481f1.svg"
+  },
+  {
+    "revision": "2da9b979719bb2e05ec4309d3a5d98f3",
+    "url": "/react-nba/static/media/BOS.2da9b979.svg"
+  },
+  {
+    "revision": "7b8ddfdd9750060f6168d8b0a849d8e9",
+    "url": "/react-nba/static/media/CHA.7b8ddfdd.svg"
+  },
+  {
+    "revision": "8f27eceb94e84da88168fbaff443b049",
+    "url": "/react-nba/static/media/CHI.8f27eceb.svg"
+  },
+  {
+    "revision": "719b6efdfed90de2cb68e49e78593ac7",
+    "url": "/react-nba/static/media/CLE.719b6efd.svg"
+  },
+  {
+    "revision": "031fe8874885c0188d685e497393981e",
+    "url": "/react-nba/static/media/DAL.031fe887.svg"
+  },
+  {
+    "revision": "d48e68ca180edf2855f38893cfd27664",
+    "url": "/react-nba/static/media/DEN.d48e68ca.svg"
+  },
+  {
+    "revision": "1ab03d43bfd35798f1c43dd60cd75656",
+    "url": "/react-nba/static/media/DET.1ab03d43.svg"
+  },
+  {
+    "revision": "61d57caa4973096b2e86bdd500b1a175",
+    "url": "/react-nba/static/media/GSW.61d57caa.svg"
+  },
+  {
+    "revision": "14eb95a312b9ef1e0648d2d98d58f46c",
+    "url": "/react-nba/static/media/HOU.14eb95a3.svg"
+  },
+  {
+    "revision": "5d8ef32eac11da5e92b7fea949caf7fc",
+    "url": "/react-nba/static/media/IND.5d8ef32e.svg"
+  },
+  {
+    "revision": "06b8ab937bee44c8e5f7f48a6a067eff",
+    "url": "/react-nba/static/media/LAC.06b8ab93.svg"
+  },
+  {
+    "revision": "ac4bf984097c8c25427017630c5f62ab",
+    "url": "/react-nba/static/media/LAL.ac4bf984.svg"
+  },
+  {
+    "revision": "9dfb78aeaa21561588e58b1fb25c86b6",
+    "url": "/react-nba/static/media/MEM.9dfb78ae.svg"
+  },
+  {
+    "revision": "1932c3dd5db28232dd7c24261b0e8798",
+    "url": "/react-nba/static/media/MIA.1932c3dd.svg"
+  },
+  {
+    "revision": "61b0a17e5c8d6ca3de13c4f254538a97",
+    "url": "/react-nba/static/media/MIL.61b0a17e.svg"
+  },
+  {
+    "revision": "2bb090ecb8ba643745c15bb3e3eecc8d",
+    "url": "/react-nba/static/media/MIN.2bb090ec.svg"
+  },
+  {
+    "revision": "92fc642d764f97f11a28de763ce233a0",
+    "url": "/react-nba/static/media/NOP.92fc642d.svg"
+  },
+  {
+    "revision": "4780d1338bcec932a42d9540c1ea36e2",
+    "url": "/react-nba/static/media/NYK.4780d133.svg"
+  },
+  {
+    "revision": "4bf80ebef87aef76f580578abbdd8861",
+    "url": "/react-nba/static/media/OKC.4bf80ebe.svg"
+  },
+  {
+    "revision": "d6ce602b19dfe1145bada5e62b60bfcc",
+    "url": "/react-nba/static/media/ORL.d6ce602b.svg"
+  },
+  {
+    "revision": "cc615a4294ce437513511e9f333b7087",
+    "url": "/react-nba/static/media/PHI.cc615a42.svg"
+  },
+  {
+    "revision": "f05ca33cd2bf8894c2eb5174c0d70012",
+    "url": "/react-nba/static/media/PHX.f05ca33c.svg"
+  },
+  {
+    "revision": "8b10743011fe911758bc725fb1f1f13c",
+    "url": "/react-nba/static/media/POR.8b107430.svg"
+  },
+  {
+    "revision": "e535f7856b24153e0f3146e8f90a45c5",
+    "url": "/react-nba/static/media/Poppins-Bold.e535f785.woff2"
+  },
+  {
+    "revision": "1a280523d375e9358d5229df34fc8e94",
+    "url": "/react-nba/static/media/Poppins-Regular.1a280523.woff2"
+  },
+  {
+    "revision": "05a8fdd5817a2e91bde4cf03c61cbefb",
+    "url": "/react-nba/static/media/SAC.05a8fdd5.svg"
+  },
+  {
+    "revision": "54a252ce4d06a33eceb8de447dd67cf3",
+    "url": "/react-nba/static/media/SAS.54a252ce.svg"
+  },
+  {
+    "revision": "46e2a8f79e72812a01e7c7cbb490250d",
+    "url": "/react-nba/static/media/TOR.46e2a8f7.svg"
+  },
+  {
+    "revision": "271453ca3efa10b044c4bbaef0e55160",
+    "url": "/react-nba/static/media/UTA.271453ca.svg"
+  },
+  {
+    "revision": "2fa3d27ee8edc074d95cbd4123e2145c",
+    "url": "/react-nba/static/media/WAS.2fa3d27e.svg"
+  }
+]);
